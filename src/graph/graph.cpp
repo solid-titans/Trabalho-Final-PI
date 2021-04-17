203766727024e611fc8c37874882d4b6ba6a2be6
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-04-17 01:33:47 -0300

    feat: added breadth first search for the graph

--- src/graph/graph.cpp
+++ src/graph/graph.cpp
@@ -1,6 +1,7 @@
 #include "graph.hpp"
 #include<iostream>
 #include <bits/stdc++.h>
+#include <array> 
 /*
 * Implementing the graph class
 *  v0.1
@@ -42,7 +43,6 @@
         return this->edges;
     }
 
-    // Add a new Vertex to the graph and return its id if it is successful
     counter Graph::add_vertex() {
         counter result = -1;
         counter pos    = this->vertices;
@@ -59,7 +59,6 @@
         return result;
     }
 
-    // Checks if the graph matrix have space, if does not, expand the matrix
     bool Graph::has_space(){
         bool result = false;
 
@@ -124,4 +123,91 @@
         std::cout << '\n'; 
 
         this->graph->print(); 
+    }
+
+    bool Graph::depth_first_search(counter first) {
+        std::vector<counter> colour;
+
+        //Mark all vertices as : "NOT VISITED"
+        for(int i = 0; i < this->vertices; i++)
+            colour.push_back(WHITE);
+
+        //Loop through all vertices in the graph
+        for(counter i = 0; i < this->vertices; i++) 
+
+            //if the curr vertex hasn't been visited
+            if(colour.at(i) == WHITE) 
+
+                //visiting current vertex
+                //Obs: I'm sending a pointer to the 
+                //memory address of the 'colour' std::vector
+                //so that all recursive calls to 'visit_vertex'
+                //can update when a vertex is visited
+                visit_vertex(i,&colour);
+
+        return true;
+    }
+
+    void Graph::visit_vertex (counter index, std::vector<counter>* colour) {
+        //Mark current vertex as visited
+        colour->at(index) = YELLOW;
+
+        //Start adj std::vector
+        std::vector<counter> curr_vertex_adj = this->adj.at(index); 
+
+        //Loop through adj std::vector of the curr_vertex
+        for(auto i = curr_vertex_adj.begin() ; i != curr_vertex_adj.end(); i++) 
+
+            //if adj vertex hasn't been visited
+            if(colour->at(*i) == WHITE)
+
+                //recursive call to 'visit_vertex'
+                visit_vertex(colour->at(*i),colour);
+        
+        //Mark vertex as completed
+        colour->at(index) = RED;
+    }
+
+    bool Graph::breadth_first_search() {
+        std::vector<counter> colour;
+
+        //Mark all vertices as : "NOT VISITED"
+        for(int i = 0; i < this->vertices; i++)
+            colour.push_back(WHITE);
+
+        //Start the queue with the first vertice of the graph
+        std::vector<counter> queue;
+        queue.push_back(0);
+
+        //While the queue is not empty
+        while(queue.size() > 0) {
+
+            //Remove the first element of the queue 
+            //and store it in a variable
+            counter curr_vert = *queue.begin();
+            queue.erase(queue.begin());
+
+            //If the element is not visited
+            if(colour.at(curr_vert) != WHITE) {
+
+                //Mark as visited
+                colour.at(curr_vert) = YELLOW;
+
+                //Start adj std::vector
+                std::vector<counter> curr_vertex_adj = this->adj.at(curr_vert); 
+
+                //Loop through adj std::vector of the curr_vertex
+                for(auto i = curr_vertex_adj.begin() ; i != curr_vertex_adj.end(); i++) 
+
+                    //if adj vertex hasn't been visited
+                    if(colour.at(*i) == WHITE)
+                        //mark to be visited in the next iterations of the loop
+                        queue.push_back(*i);
+
+                //mark the curr_vertex as completed
+                colour.at(curr_vert) = RED;
+            }
+        }
+
+        return true;
     }
--- src/graph/graph.hpp
+++ src/graph/graph.hpp
@@ -3,6 +3,10 @@
 
 #define EMPTY -1
 
+#define WHITE  0
+#define YELLOW 1
+#define RED    2
+
 #include <vector>
 #include "matrix_cpp/matrix.cpp"
 
@@ -12,32 +16,75 @@ using counter = uint16_t;
 class Graph {
 
     private: 
+
+        //Attributes
         counter vertices;
         counter edges;
 
-        Matrix<weight>*       graph;
+
+        Matrix<weight>* graph;
         std::vector<std::vector<counter>> adj;
 
+        //Functions:
+
+        // Checks if the graph matrix have space, if does not, expand the matrix
         bool has_space();
+        // Add a 'last' vertex to be adj to 'first'
+        void add_adj(counter first,counter last);
+        // Visit a specific vertex and verify the color of the adjs
+        //This is a function only used in depth first search
+        void visit_vertex(counter index, std::vector<counter>* colour);
 
     public:
         
-        //
+        //Constructors
+
+        //Start the Graph with three empty vertices
         Graph();
+        //Start the Graph with 'vertices' number of vertices
         Graph(counter vertices);
+        //Descontructor
         ~Graph();
+
+        //Basic graph operations
         
-        //
-        bool    add_edge(counter first, counter last, weight weight = 0);
+        /** Add a new Edge, by giving the coordenates of the first and last vertex
+         *  (Obs: There is also the option of adding a weight to the 
+         *  Edge, but if not specified, the 'weight' will be 0 )
+         */ 
+        bool add_edge(counter first, counter last, weight weight = 0);
+        // Add a new Vertex to the graph and return if it is successful 
         counter add_vertex();
-        bool    search_vertex(counter id);
-        void    add_adj(counter first,counter last);
-        
-        //
+        //Make a basic search with the id of the vertex
+        bool search_vertex(counter id);
+        // get number of vertices of the graph
         counter vertices_num();  
+        // get number of edges of the graph
         counter edges_num();
-        
+        // Print all data about the graph
         virtual void print();  
+
+        // Special algorithms
+
+        /** Make a Depth First Search in the Graph
+         * 
+         *  Significance of the colours:
+         * 
+         *  WHITE: Not explored
+         *  YELLOW: Visited, but there's still adjacents to visit 
+         *  RED: Visited, along with the adjacents
+         */
+        bool depth_first_search(counter first);
+
+        /** Make a Breadth First Search in the Graph
+         * 
+         *  Significance of the colours:
+         * 
+         *  WHITE: Not explored
+         *  YELLOW: Visited, but there's still adjacents to visit 
+         *  RED: Visited, along with the adjacents
+         */
+        bool breadth_first_search();
 };
 
 #endif
