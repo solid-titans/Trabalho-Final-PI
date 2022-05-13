759d0e7f414796e316693d2da319cdca04f6d454
GustavoLR548 <gustavolr035@gmail.com>
2022-05-12 23:03:34 -0300

    hfix: corrigido erro no image_processor

--- trabalho-final-pi/widgets/image/image_processor.py
+++ trabalho-final-pi/widgets/image/image_processor.py
@@ -8,30 +8,10 @@ class ImageProcessor(QWidget):
         QWidget.__init__(self,*args,**kwargs)
 
     def set_image(file_path):
-
+        pass
 
     #@Slot
-    def plot_image(self,file_path):
+    def make_sharpen(self):
+        result = ImageProcessorUtils.sharpen(self.image,10,3)
 
-        image = cv2.imread(file_path)
-        image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 
-        glcm = graycomatrix(image, [1], [0], 256, symmetric=False, normed=True)
-
-        xs = []
-        ys = []
-
-        xs.append(graycoprops(glcm, 'dissimilarity')[0, 0])
-        ys.append(graycoprops(glcm, 'correlation')[0, 0])
-
-        static_canvas = FigureCanvas(Figure(figsize=(5, 3)))
-
-        self.remove_children()
-
-        self.layout.addWidget(NavigationToolbar(static_canvas, self))
-        self.layout.addWidget(static_canvas)
-
-        self._static_ax = static_canvas.figure.subplots()
-
-        self._static_ax.imshow(glcm)
-        self._static_ax.show()
