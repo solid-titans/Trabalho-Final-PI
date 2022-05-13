1f1b42c8213964b1bca21eb8d4ac2afeca239a52
Homenique Vieira <homenique.t@gmail.com>
2022-05-13 00:47:38 -0300

    [update] feito a função de equalization

--- trabalho-final-pi/utils/image_processor_utils.py
+++ trabalho-final-pi/utils/image_processor_utils.py
@@ -11,8 +11,13 @@ def brightness_and_contrast(image,parameters):
 def gaussian_blur(image,ksize =(3,3),border = cv.BORDER_DEFAULT):
     return cv.GaussianBlur(image,ksize,border)
 
-def equalization(image,parameters):
-    pass
+def equalization(image):
+    R, G, B = cv.split(img)
+
+    output1_R = cv.equalizeHist(R)
+    output1_G = cv.equalizeHist(G)
+    output1_B = cv.equalizeHist(B)
+    return cv.merge((output1_R, output1_G, output1_B))
 
 def select_image_area(image,xi,yi,xf,yf):
     pass
@@ -21,7 +26,7 @@ def select_image_area(image,xi,yi,xf,yf):
 if __name__ == "__main__":
     img = cv.imread('../assets/imgs/imagem1.jpg')
     cv.imshow('Original', img)
-    cv.imshow('saida 3x3',gaussian_blur(img,ksize=(7,7)))
-    cv.imshow('saida 7x7',gaussian_blur(img))
-    cv.imshow('saida 15x15',gaussian_blur(img,ksize=(15,15)))
+
+    cv.imshow('Equalization', equalization(img))
+
     cv.waitKey(0)
