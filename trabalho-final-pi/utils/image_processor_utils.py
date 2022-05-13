42e13de87104e4f6904dcbdeffffa594ed2732b6
Homenique Vieira <homenique.t@gmail.com>
2022-05-12 23:45:05 -0300

    [update] Feito a função de gaussian

--- trabalho-final-pi/utils/image_processor_utils.py
+++ trabalho-final-pi/utils/image_processor_utils.py
@@ -1,14 +1,27 @@
 # This Python file uses the following encoding: utf-8
-import cv2
+import cv2 as cv
 
-def sharpen(image,parameters):
+def sharpen(image,parameters=0):
     pass
 
 def brightness_and_contrast(image,parameters):
     pass
 
-def gaussian_blur(image,parameters):
-    pass
+
+def gaussian_blur(image,ksize =(3,3),border = cv.BORDER_DEFAULT):
+    return cv.GaussianBlur(image,ksize,border)
 
 def equalization(image,parameters):
     pass
+
+def select_image_area(image,xi,yi,xf,yf):
+    pass
+
+
+if __name__ == "__main__":
+    img = cv.imread('../assets/imgs/imagem1.jpg')
+    cv.imshow('Original', img)
+    cv.imshow('saida 3x3',gaussian_blur(img,ksize=(7,7)))
+    cv.imshow('saida 7x7',gaussian_blur(img))
+    cv.imshow('saida 15x15',gaussian_blur(img,ksize=(15,15)))
+    cv.waitKey(0)
