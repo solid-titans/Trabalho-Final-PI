c1c683a64bd1fbfc7f0073bfbdb106e37c8649df
GustavoLR548 <gustavolr035@gmail.com>
2022-05-12 23:02:34 -0300

    feat: adicionado image_processor utils

---
+++ trabalho-final-pi/utils/image_processor_utils.py
@@ -0,0 +1,14 @@
+# This Python file uses the following encoding: utf-8
+import cv2
+
+def sharpen(image,parameters):
+    pass
+
+def brightness_and_contrast(image,parameters):
+    pass
+
+def gaussian_blur(image,parameters):
+    pass
+
+def equalization(image,parameters):
+    pass
